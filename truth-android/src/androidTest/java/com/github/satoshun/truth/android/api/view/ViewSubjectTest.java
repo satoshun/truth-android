8f889eec5539a96f3659f94a945cd4cdaccc0840
SatoShun <shun.sato1@gmail.com>
2016-11-10 03:46:14 +0000

    add first test

--- truth-android/src/androidTest/java/com/github/satoshun/truth/android/api/view/ViewSubjectTest.java
+++ truth-android/src/androidTest/java/com/github/satoshun/truth/android/api/view/ViewSubjectTest.java
@@ -1,16 +1,41 @@
 package com.github.satoshun.truth.android.api.view;
 
+import android.support.test.InstrumentationRegistry;
+import android.support.test.annotation.UiThreadTest;
+import android.support.test.rule.UiThreadTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+
+import com.github.satoshun.truth.android.api.TruthAndroid;
+
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
-
-/**
- * Created by stsn on 11/10/16.
- */
+@RunWith(AndroidJUnit4.class)
 public class ViewSubjectTest {
-  @Test
-  public void isVisible() throws Exception {
 
+  @Rule public UiThreadTestRule uiThreadTestRule = new UiThreadTestRule();
+
+  private View view;
+
+  @Before
+  public void setUp() throws Exception {
+    view = new View(InstrumentationRegistry.getTargetContext());
   }
 
+  @UiThreadTest @Test
+  public void isVisible_success() throws Exception {
+    view.setVisibility(View.VISIBLE);
+    TruthAndroid.assertThat(view)
+            .isVisible();
+  }
+
+  @UiThreadTest @Test(expected = AssertionError.class)
+  public void isVisible_failure() throws Exception {
+    view.setVisibility(View.INVISIBLE);
+    TruthAndroid.assertThat(view)
+            .isVisible();
+  }
 }
