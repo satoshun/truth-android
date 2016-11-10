bd967b9e1f288cc15fd037850fc9e97ef2a2200a
SatoShun <shun.sato1@gmail.com>
2016-11-10 04:05:37 +0000

    add some method to ViewSubject

--- truth-android/src/androidTest/java/com/github/satoshun/truth/android/api/view/ViewSubjectTest.java
+++ truth-android/src/androidTest/java/com/github/satoshun/truth/android/api/view/ViewSubjectTest.java
@@ -1,5 +1,6 @@
 package com.github.satoshun.truth.android.api.view;
 
+import android.os.Build;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.annotation.UiThreadTest;
 import android.support.test.rule.UiThreadTestRule;
@@ -12,6 +13,9 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.os.Build.VERSION_CODES.HONEYCOMB;
+import static com.google.common.truth.TruthJUnit.assume;
 
 @RunWith(AndroidJUnit4.class)
 public class ViewSubjectTest {
@@ -26,6 +30,22 @@ public class ViewSubjectTest {
   }
 
   @UiThreadTest @Test
+  public void isSameAlpha_success() throws Exception {
+    assumeVersion(HONEYCOMB);
+    view.setAlpha((float) 0.45);
+    TruthAndroid.assertThat(view)
+            .isSameAlpha((float) 0.45);
+  }
+
+  @UiThreadTest @Test(expected = AssertionError.class)
+  public void isSameAlpha_failure() throws Exception {
+    assumeVersion(HONEYCOMB);
+    view.setAlpha((float) 0.45);
+    TruthAndroid.assertThat(view)
+            .isSameAlpha((float) 0.451);
+  }
+
+  @UiThreadTest @Test
   public void isVisible_success() throws Exception {
     view.setVisibility(View.VISIBLE);
     TruthAndroid.assertThat(view)
@@ -37,5 +57,24 @@ public class ViewSubjectTest {
     view.setVisibility(View.INVISIBLE);
     TruthAndroid.assertThat(view)
             .isVisible();
+  }
+
+  @UiThreadTest @Test
+  public void isNotVisible_success() throws Exception {
+    view.setVisibility(View.GONE);
+    TruthAndroid.assertThat(view)
+            .isNotVisible();
+  }
+
+  @UiThreadTest @Test(expected = AssertionError.class)
+  public void isNotVisible_failure() throws Exception {
+    view.setVisibility(View.VISIBLE);
+    TruthAndroid.assertThat(view)
+            .isNotVisible();
+  }
+
+  private static void assumeVersion(int versionCode) {
+    assume().that(Build.VERSION.SDK_INT)
+            .isGreaterThan(versionCode);
   }
 }
--- truth-android/src/main/java/com/github/satoshun/truth/android/api/view/ViewSubject.java
+++ truth-android/src/main/java/com/github/satoshun/truth/android/api/view/ViewSubject.java
@@ -1,10 +1,13 @@
 package com.github.satoshun.truth.android.api.view;
 
+import android.annotation.TargetApi;
 import android.view.View;
 
 import com.google.common.truth.FailureStrategy;
 import com.google.common.truth.Subject;
 import com.google.common.truth.SubjectFactory;
+
+import static android.os.Build.VERSION_CODES.HONEYCOMB;
 
 public class ViewSubject extends Subject<ViewSubject, View> {
 
@@ -14,12 +17,29 @@ public class ViewSubject extends Subject<ViewSubject, View> {
     super(failureStrategy, actual);
   }
 
+  @TargetApi(HONEYCOMB)
+  public ViewSubject isSameAlpha(float alpha) {
+    if (actual().getAlpha() != alpha) {
+      fail("is same alpha", actual().getAlpha());
+    }
+    return this;
+  }
+
   public ViewSubject isVisible() {
     isNotNull();
     if (actual().getVisibility() != View.VISIBLE) {
       fail("is visible", actual().getVisibility());
     }
     return this;
+  }
+
+  public ViewSubject isNotVisible() {
+    isNotNull();
+    if (actual().getVisibility() == View.VISIBLE) {
+      fail("is not visible", actual().getVisibility());
+    }
+    return this;
+
   }
 
   private static class ViewSubjectFactory extends SubjectFactory<ViewSubject, View> {
