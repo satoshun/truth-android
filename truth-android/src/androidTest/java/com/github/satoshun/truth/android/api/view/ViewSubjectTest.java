7140ecce1e1b84400e36a9559b39bd0369f602df
SatoShun <shun.sato1@gmail.com>
2016-11-10 03:43:43 +0000

    add LICENSE: apache 2.0

---
+++ truth-android/src/androidTest/java/com/github/satoshun/truth/android/api/view/ViewSubjectTest.java
@@ -0,0 +1,16 @@
+package com.github.satoshun.truth.android.api.view;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by stsn on 11/10/16.
+ */
+public class ViewSubjectTest {
+  @Test
+  public void isVisible() throws Exception {
+
+  }
+
+}
