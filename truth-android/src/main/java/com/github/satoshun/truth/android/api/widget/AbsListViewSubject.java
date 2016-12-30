fa41751e7ad055ee06c46b25fb9ac74ca57cd58f
SatoShun <shun.sato1@gmail.com>
2016-12-30 09:13:12 +0000

    AbsListViewSubject

--- truth-android/src/main/java/com/github/satoshun/truth/android/api/widget/AbsListViewSubject.java
+++ truth-android/src/main/java/com/github/satoshun/truth/android/api/widget/AbsListViewSubject.java
@@ -1,9 +1,15 @@
 package com.github.satoshun.truth.android.api.widget;
 
+import android.annotation.TargetApi;
 import android.widget.AbsListView;
 
 import com.google.common.truth.FailureStrategy;
 import com.google.common.truth.SubjectFactory;
+
+import java.util.Arrays;
+
+import static android.os.Build.VERSION_CODES.HONEYCOMB;
+import static android.os.Build.VERSION_CODES.KITKAT;
 
 /**
  * Propositions for AbsListView subject
@@ -15,6 +21,42 @@ public abstract class AbsListViewSubject<S extends AbsListViewSubject<S, T>, T e
 
   AbsListViewSubject(FailureStrategy failureStrategy, T actual) {
     super(failureStrategy, actual);
+  }
+
+  @TargetApi(HONEYCOMB)
+  public S isCheckedItemsCount(int count) {
+    isNotNull();
+    check().withFailureMessage("is checked items count")
+        .that(actual().getCheckedItemCount())
+        .isEqualTo(count);
+    return myself();
+  }
+
+  @TargetApi(HONEYCOMB)
+  public S containsItemIds(long... itemIds) {
+    isNotNull();
+    check().withFailureMessage("contains item ids")
+        .that(actual().getCheckedItemIds())
+        .isIn(Arrays.asList(itemIds));
+    return myself();
+  }
+
+  @TargetApi(HONEYCOMB)
+  public S isCheckedItemPosition(int position) {
+    isNotNull();
+    check().withFailureMessage("is checked item position")
+        .that(actual().getCheckedItemPosition())
+        .isEqualTo(position);
+    return myself();
+  }
+
+  @TargetApi(KITKAT)
+  public S canScrollList(int direction) {
+    isNotNull();
+    check().withFailureMessage("can scroll list")
+        .that(actual().canScrollList(direction))
+        .isTrue();
+    return myself();
   }
 
   private static class AbsListViewSubjectImpl extends AbsListViewSubject<AbsListViewSubjectImpl, AbsListView> {
