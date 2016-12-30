d57a770a952b4bcf55323b53d7c6cc96feff25d9
SatoShun <shun.sato1@gmail.com>
2016-12-30 12:19:46 +0000

    ListViewSubject

--- truth-android/src/main/java/com/github/satoshun/truth/android/api/widget/ListViewSubject.java
+++ truth-android/src/main/java/com/github/satoshun/truth/android/api/widget/ListViewSubject.java
@@ -1,9 +1,13 @@
 package com.github.satoshun.truth.android.api.widget;
 
+import android.annotation.TargetApi;
+import android.graphics.drawable.Drawable;
 import android.widget.ListView;
 
 import com.google.common.truth.FailureStrategy;
 import com.google.common.truth.SubjectFactory;
+
+import static android.os.Build.VERSION_CODES.KITKAT;
 
 /**
  * Propositions for ListView subject
@@ -15,6 +19,90 @@ public abstract class ListViewSubject<S extends ListViewSubject<S, T>, T extends
 
   public ListViewSubject(FailureStrategy failureStrategy, T actual) {
     super(failureStrategy, actual);
+  }
+
+  public S isDivider(Drawable divider) {
+    isNotNull();
+    check().withFailureMessage("is divider")
+        .that(actual().getDivider())
+        .isSameAs(divider);
+    return myself();
+  }
+
+  public S isDividerHeight(int height) {
+    isNotNull();
+    check().withFailureMessage("is divider height")
+        .that(actual().getDividerHeight())
+        .isEqualTo(height);
+    return myself();
+  }
+
+  public S isFooterViewsCount(int count) {
+    isNotNull();
+    check().withFailureMessage("is footer views count")
+        .that(actual().getFooterViewsCount())
+        .isEqualTo(count);
+    return myself();
+  }
+
+  public S isHeaderViewsCount(int count) {
+    isNotNull();
+    check().withFailureMessage("is header views count")
+        .that(actual().getHeaderViewsCount())
+        .isEqualTo(count);
+    return myself();
+  }
+
+  public S isOverscrollFooter(Drawable footer) {
+    isNotNull();
+    check().withFailureMessage("is overscroll footer")
+        .that(actual().getOverscrollFooter())
+        .isSameAs(footer);
+    return myself();
+  }
+
+  public S isOverscrollHeader(Drawable header) {
+    isNotNull();
+    check().withFailureMessage("is overscroll header")
+        .that(actual().getOverscrollHeader())
+        .isSameAs(header);
+    return myself();
+  }
+
+  @TargetApi(KITKAT)
+  public S isFooterDividersEnabled() {
+    isNotNull();
+    check().withFailureMessage("is footer dividers enabled")
+        .that(actual().areFooterDividersEnabled())
+        .isTrue();
+    return myself();
+  }
+
+  @TargetApi(KITKAT)
+  public S isFooterDividersDisabled() {
+    isNotNull();
+    check().withFailureMessage("is footer dividers disabled")
+        .that(actual().areFooterDividersEnabled())
+        .isFalse();
+    return myself();
+  }
+
+  @TargetApi(KITKAT)
+  public S isHeaderDividersEnabled() {
+    isNotNull();
+    check().withFailureMessage("is header dividers enabled")
+        .that(actual().areHeaderDividersEnabled())
+        .isTrue();
+    return myself();
+  }
+
+  @TargetApi(KITKAT)
+  public S isHeaderDividersDisabled() {
+    isNotNull();
+    check().withFailureMessage("is header dividers disabled")
+        .that(actual().areHeaderDividersEnabled())
+        .isFalse();
+    return myself();
   }
 
   private static class ListViewSubjectImpl extends ListViewSubject<ListViewSubjectImpl, ListView> {
